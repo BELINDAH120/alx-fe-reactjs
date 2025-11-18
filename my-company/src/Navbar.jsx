d758793b10cf62b71594e9b05933cdd06fef0c4f
BELINDAH120 <128031203+BELINDAH120@users.noreply.github.com>
2025-11-18 10:30:35 +0100

    Create Navbar.jsx

---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,33 @@
+// Navbar.jsx
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        backgroundColor: "navy",
+        padding: "15px",
+        display: "flex",
+        gap: "20px",
+      }}
+    >
+      <Link style={{ color: "white", textDecoration: "none" }} to="/">
+        Home
+      </Link>
+
+      <Link style={{ color: "white", textDecoration: "none" }} to="/about">
+        About
+      </Link>
+
+      <Link style={{ color: "white", textDecoration: "none" }} to="/services">
+        Services
+      </Link>
+
+      <Link style={{ color: "white", textDecoration: "none" }} to="/contact">
+        Contact
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
